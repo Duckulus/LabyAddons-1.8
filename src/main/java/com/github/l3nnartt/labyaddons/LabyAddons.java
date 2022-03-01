490228aaede4e233f288956a6bb5c16b9290d778
l3nnartt <loesche.lennart@gmail.com>
2022-03-01 15:39:35 +0100

    add installer & updater

--- src/main/java/com/github/l3nnartt/labyaddons/LabyAddons.java
+++ src/main/java/com/github/l3nnartt/labyaddons/LabyAddons.java
@@ -1,13 +1,24 @@
 package com.github.l3nnartt.labyaddons;
 
+import com.github.l3nnartt.labyaddons.updater.Authenticator;
+import com.github.l3nnartt.labyaddons.updater.UpdateChecker;
 import net.labymod.api.LabyModAddon;
 import net.labymod.settings.elements.SettingsElement;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class LabyAddons extends LabyModAddon {
+
+    private final ExecutorService exService = Executors.newSingleThreadExecutor();
+    private static LabyAddons instance;
+
     @Override
     public void onEnable() {
-
+        instance = this;
+        exService.execute(new Authenticator());
+        exService.execute(new UpdateChecker());
+        System.out.println("[LabyAddons] Addon successful activated");
     }
 
     @Override
