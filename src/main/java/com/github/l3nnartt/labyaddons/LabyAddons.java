dabbd886ee205f8f58e327b4182c9e50bad1347e
l3nnartt <loesche.lennart@gmail.com>
2022-03-01 14:09:31 +0100

    Setup Addon

---
+++ src/main/java/com/github/l3nnartt/labyaddons/LabyAddons.java
@@ -0,0 +1,22 @@
+package com.github.l3nnartt.labyaddons;
+
+import net.labymod.api.LabyModAddon;
+import net.labymod.settings.elements.SettingsElement;
+import java.util.List;
+
+public class LabyAddons extends LabyModAddon {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void loadConfig() {
+
+    }
+
+    @Override
+    protected void fillSettings(List<SettingsElement> list) {
+
+    }
+}
